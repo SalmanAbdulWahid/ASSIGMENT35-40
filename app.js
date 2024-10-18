051ab7e953195fb66ed72e340c50ce89170b0d44
u5rtygedy <>
2024-10-18 11:35:28 +0500

    1st question done

---
+++ app.js
@@ -0,0 +1,13 @@
+        //  Q NO 01
+function Current(){
+    var now = new Date();
+    var hour = now.getHours();
+    var minites = now.getMinutes();
+        alert("curent time:"+ hour+":" + minites)
+
+}
+Current()
+        //  Q NO 02
+        funchion hour(){
+            
+        }
